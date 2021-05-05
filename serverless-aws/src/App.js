fbbf0966d73f5fbe553e972191a5dc15dadb2cc0
Chad Kaplan <chadkaplan@knights.ucf.edu>
2021-05-05 03:52:32 -0400

    Added some basic JSX elements and state for testing

--- serverless-aws/src/App.js
+++ serverless-aws/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = { 
+    isLoading :false,
+    invoices : []
+    }
 
-export default App;
+    render() { 
+        const isLoading = this.state.isLoading;
+        const invoices = this.state.invoices;
+        const body=[
+            {
+                "id" : "100",
+                    "Vendor" : "Nomad",
+                    "Amount" : 18000,
+                    "Date" : "10/14/1992"
+                },
+                {
+                    "id" : "100",
+                    "Vendor" : "Nomad",
+                    "Amount" : 18000,
+                    "Date" : "10/14/1992"
+                },
+                {
+                    "id" : "100",
+                    "Vendor" : "Nomad",
+                    "Amount" : 18000,
+                    "Date" : "10/14/1992"
+                }
+            ]
+            if (isLoading)
+            return(<div>Loading...</div>)
+
+            return ( 
+                <div className="container border border-secondary rounded center">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>Pending Invoices - The Test Company</h4>
+                        </div>
+                    </div>
+
+
+                </div>
+            );
+        }
+    }
+    
+    export default App;
