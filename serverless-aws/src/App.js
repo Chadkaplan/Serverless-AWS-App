79631af8f1c052484706fe6daf45a7b95a0eb884
Chad Kaplan <chadkaplan@knights.ucf.edu>
2021-05-12 18:33:44 -0400

    Finsihed front end, need to add backend using AWS

--- serverless-aws/src/App.js
+++ serverless-aws/src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { Table, Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faThumbsDown,
+  faThumbsUp,
+  faImage,
+  faMoneyCheckAlt,
+  faSearchDollar,
+} from "@fortawesome/free-solid-svg-icons";
 
 class App extends Component {
   state = {
     isLoading: false,
     invoices: [
       {
-        id: "100",
+        id: 1,
         vendor: "Nomad",
         amount: 17000,
         invoice: 1234,
         date: "10/14/1992",
       },
       {
-        id: "200",
+        id: 2,
         vendor: "Omad",
         amount: 18000,
         invoice: 5678,
         date: "10/15/1992",
       },
       {
-        id: "300",
+        id: 3,
         vendor: "Mad",
         amount: 19000,
         invoice: 9101,
@@ -28,32 +36,41 @@ class App extends Component {
       },
     ],
   };
+
   remove(id) {
     console.log(id);
-    let updatedInvoices = { ...this.state.invoices }.filter(i => i.id !== id);
+    let updatedInvoices = [...this.state.invoices].filter((i) => i.id !== id);
+
     this.setState({ invoices: updatedInvoices });
+  }
+
+  async componentDidMount() {
+    const response = await fetch(
+      "https://e106ldopga.execute-api.us-west-1.amazonaws.com/Dev"
+    );
+    const body = await response.json();
+    this.setState({ invoices: body, isLoading: false });
   }
 
   render() {
     const isLoading = this.state.isLoading;
-    const allInvoices = this.state.invoices;
+    const allinvoices = this.state.invoices;
 
     if (isLoading) return <div>Loading...</div>;
 
-    let invoices = allInvoices.map((invoice) => (
+    let invoices = allinvoices.map((invoice) => (
       <tr key={invoice.id}>
-        <td>{invoice.id}</td>
-        <td>{invoice.vendor}</td>
-        <td>{invoice.amount}</td>
+        <td>{invoice.Vendor}</td>
+        <td>{invoice.Amount}</td>
         <td>{invoice.invoice}</td>
-        <td>{invoice.date}</td>
+        <td>{invoice.Date}</td>
         <td>
           <Button
             className="btn btn-lg btn-success"
             onClick={() => this.remove(invoice.id)}
           >
             {" "}
-            OK{" "}
+            <FontAwesomeIcon icon={faThumbsUp} /> OK{" "}
           </Button>
         </td>
         <td>
@@ -62,7 +79,7 @@ class App extends Component {
             onClick={() => this.remove(invoice.id)}
           >
             {" "}
-            NOK{" "}
+            <FontAwesomeIcon icon={faThumbsDown} /> NOK{" "}
           </Button>
         </td>
         <td>
@@ -71,7 +88,7 @@ class App extends Component {
             onClick={() => this.remove(invoice.id)}
           >
             {" "}
-            50%{" "}
+            <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50%{" "}
           </Button>
         </td>
         <td>
@@ -79,8 +96,7 @@ class App extends Component {
             className="btn btn-lg btn-warning"
             onClick={() => this.remove(invoice.id)}
           >
-            {" "}
-            ??{" "}
+            <FontAwesomeIcon icon={faSearchDollar} /> ??{" "}
           </Button>
         </td>
         <td>
@@ -89,40 +105,44 @@ class App extends Component {
             onClick={() => this.remove(invoice.id)}
           >
             {" "}
-            Image{" "}
+            <FontAwesomeIcon icon={faImage} /> Image{" "}
           </Button>
         </td>
       </tr>
     ));
 
     return (
-      <div>
-        <div className="container border border-secondary rounded center">
-          <div className="row">
-            <div className="col-9">
-              <h4> Pending Invoices - The Test Company</h4>
-            </div>
+      <div className="container border border-secondary rouded center">
+        <div className="row">
+          <div className="col-12">
+            <h4>Pending Invoices - The Test Comapny</h4>
           </div>
         </div>
-        <div className="col-xs-12 center text-center"></div>
-        <Table dark responsive striped bordered hover>
-          <thead>
-            <tr>
-              <th>Vendor</th>
-              <th>Amount</th>
-              <th>Invoice #</th>
-              <th colSpan="4">Actions</th>
-              <th>Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.invoices.length === 0 ? (
-              <td colSpan="9">All caught up!</td>
-            ) : (
-              invoices
-            )}
-          </tbody>
-        </Table>
+
+        <div className="row">
+          <div className=".col-xs-12 center text-center">
+            <Table dark responsive striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Vendor</th>
+                  <th>Amount</th>
+                  <th>Invoice #</th>
+                  <th>Date</th>
+                  <th colSpan="4">Actions</th>
+                  <th>Image</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {this.state.invoices.length === 0 ? (
+                  <td colSpan="9">All caught up!</td>
+                ) : (
+                  invoices
+                )}
+              </tbody>
+            </Table>
+          </div>
+        </div>
       </div>
     );
   }
